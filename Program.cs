c08d21dc1cebcff576b1bd74a10bccd3d158c3d2
Rostyslav Malanchuk <rostyslavmalanchuk15@gmail.com>
2025-12-20 23:37:04 +0100

    k

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace CVBuddy
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+            }//mothefatha
 
             app.UseHttpsRedirection();
             app.UseRouting();
