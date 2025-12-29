1b518c9d10782b8a6a8450e84dbf9646f6f1674a
Rostyslav Malanchuk <rostyslavmalanchuk15@gmail.com>
2025-12-29 20:37:54 +0100

    validerat lite lösenord

--- Models/ChangePassword.cs
+++ Models/ChangePassword.cs
@@ -4,13 +4,16 @@ namespace CVBuddy.Models
 {
     public class ChangePassword
     {
-        [Required]
+        [Required(ErrorMessage ="Please put your old password")]
         [DataType(DataType.Password)]
         public string CurrentPassword { get; set; }
-        [Required]
+
+        [Required(ErrorMessage ="Please put your old password")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$", ErrorMessage = "Password should contain at least: 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number")]
         [DataType(DataType.Password)]
         public string NewPassword { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please put your old password")]
         [DataType(DataType.Password)]
         [Compare("NewPassword")]
         public string ConfirmPassword { get; set; }
