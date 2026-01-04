21352b5d21f20233dcd85778332cf2a77baf8176
kanatdavid <david.kanat@hotmail.com>
2026-01-04 14:30:08 +0100

    message routing klar

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CVBuddy.Models;
+
+namespace CVBuddy.Controllers
+{
+    public class MessageController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateMsg()
+        {
+            return View(new Message());
+        }
+    }
+}
