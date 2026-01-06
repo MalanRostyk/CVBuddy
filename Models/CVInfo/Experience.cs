3e918ec2c71194c29ea953d4ea434661919a68b6
kanatdavid <david.kanat@hotmail.com>
2026-01-07 00:05:40 +0100

    YEEEEEES!!!!! Validering create CV funkar

--- Models/CVInfo/Experience.cs
+++ Models/CVInfo/Experience.cs
@@ -8,7 +8,7 @@ namespace CVBuddy.Models.CVInfo
         [Key]
         public int Exid { get; set; }
 
-        [Required(ErrorMessage = "You need to enter what the experience title is, don't leave empty")] //Få ha alla tecken vid fall att "ASP.NET" eller "Fork-lift license"
+        [Required(ErrorMessage = "You need to enter what the experience title is, don't leave empty.")] //Få ha alla tecken vid fall att "ASP.NET" eller "Fork-lift license"
         [StringLength(90, MinimumLength = 2)]
         public string Title { get; set; }
 
@@ -16,11 +16,11 @@ namespace CVBuddy.Models.CVInfo
         [StringLength(120)]
         public string? Description { get; set; }
 
-        [Required(ErrorMessage = "Don't leave empty, if it was a freelance experience enter ''Freelance''")] //Få ha alla tecken vid fall att "ASP.NET" eller "Fork-lift license"
+        [Required(ErrorMessage = "Don't leave company field empty, if it was a freelance experience enter ''Freelance''.")] //Få ha alla tecken vid fall att "ASP.NET" eller "Fork-lift license"
         [StringLength(90, MinimumLength = 2)]
         public string Company{ get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "An added experience must have a start date, dont leave unentered.")]
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
 
