81417a328e5874861e947811d436031bb5fd1ffe
kanatdavid <david.kanat@hotmail.com>
2026-01-08 22:51:20 +0100

    kan radera ett meddelande

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -82,5 +82,42 @@ namespace CVBuddy.Controllers
                 return NotFound("Message could not be found.");
             return View(msg);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteMessage(int mid)
+        {
+            //Undantagshantering ska göras för exceptionella situationer, det ska inte funka som en if sats för att fånga null värden här och var
+            //utan det ska hanteras med kontroller, alltså en if-sats. Och då skriver vi ut ett informativt felmeddelande som ska ses av användare.
+            //Exempelvis som här nedan, kontroller ska användas för att validera inputs(bortse från att int mid är ett routeat id). Hittas inte det som efterfrågas
+            //så meddelar vi användaren om det. Det är alltså FÖRVÄNTADE SCENARION. Men när det gäller undantagshantering så innebär det att
+            //att man ska fånga verkliga fel och ge begripliga och beskrivande felmeddelanden istället för att låta applikationen krascha
+            try
+            {
+                //Hitta meddelandet med mid
+                var message = await _context.Messages.FirstOrDefaultAsync(m => m.Mid == mid);
+
+                
+                if (message == null)
+                    return NotFound("The message you want to delete could not be found.");
+                
+                //Radera
+                _context.Messages.Remove(message);
+
+                //Savechangesasync
+                await _context.SaveChangesAsync();
+
+                //return RedirectToAction IEnumerable<Message> som tillhör användaren
+                var userId = _userManager.GetUserId(User);
+                List<Message> usersMessages = await _context.Messages.Where(m => m.RecieverId == userId).ToListAsync();
+                
+                return RedirectToAction("Messages", usersMessages);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, "An issue occured while trying to remove the message.");//StatusCode 500 betyder internal error.
+            }
+            
+        }
+
     }
 }
