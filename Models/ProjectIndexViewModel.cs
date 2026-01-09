57c1355be1b1e914bf271b2209eaa9f841ac7d21
Rostyslav Malanchuk <rostyslavmalanchuk15@gmail.com>
2026-01-09 03:43:35 +0100

    för imorgon
    testa alla variabler som filtrerar listorna i view lägga i controller, skapa fält för de här, skapa uservm och projektvm tilltela med dto, anropa sedan fält från view via Model.

--- Models/ProjectIndexViewModel.cs
+++ Models/ProjectIndexViewModel.cs
@@ -6,7 +6,7 @@
         public List<Project> OtherProjects { get; set; } = new();
         public List<Project> PublicProjects { get; set; } = new();//ändring
         //testa alla variabler som filtrerar listorna i view lägga i controller, skapa fält för de här,
-        //skapa uservm och projektvm tilltela med dto, anropa sedan fält från view via Model
+        //skapa uservm och projektvm tilltela med dto, anropa sedan fält från view via Model.
 
     }
 }
